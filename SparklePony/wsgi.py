cdb1a51db595ebec9b4f7f35227417778e42afa4
Sue White <whitesue@amazon.com>
2017-06-28 12:06:19 -0700

    initial commit, blank django project

---
+++ SparklePony/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SparklePony project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SparklePony.settings")
+
+application = get_wsgi_application()
